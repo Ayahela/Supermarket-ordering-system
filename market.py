dcf7cec4fa1ea58c9d15b6351aca84badb18d632
Alaa Emam <aslamamam609@gmail.com>
2025-07-24 13:14:29 +0200

    Update market.py

--- market.py
+++ market.py
@@ -10,9 +10,42 @@ pro.geometry('800x450+280+50')
 pro.resizable(False, False)
 pro.title('Supermarket')
 pro.iconbitmap(r'C:\super.ico')
+
 # العنوان
 title = Label(pro, text='Super Market System', fg='yellow', bg='black', font=('Arial', 20, 'bold'))
 title.pack(fill='x')
+
+
+#forth video(fun of each button)
+u1='https://www.facebook.com/onlineshop00432'
+u2='https://t.me/digitalearningwithprishangwithprisha'
+u3='https://www.youtube.com/shorts/8KSMwP2fbGg'
+def Open1():
+    webbrowser.open_new(u1)
+def OPen2():
+    webbrowser.open_new(u2)
+def OPen3():
+    webbrowser.open_new(u3)    
+def about1():
+    messagebox.showinfo('تيم خليها على الله','develepoer')
+def about2():
+    messagebox.showinfo('project','supermarket project on tinkerkat with python:)')      
+
+def log():
+    user=En1.get()
+    password=En2.get()
+    if user == 'BFCAI' and password=='supermarket':
+        messagebox.showinfo('hello','WelcomeBack ')
+    else:
+        messagebox.showerror('error','sorry passwoed in wrong')    
+        
+
+
+
+
+
+
+
 
 # المستطيل الأحمر
 F1 = Frame(pro, width=230, height=500, bg='#0B2F3A')
@@ -23,26 +56,53 @@ Title2=Label(F1,text='Developed by:Team',bg='#0B2F3A',fg='white',font=('Arial',1
 Title2.place(x=25,y=50)
 Title3=Label(F1,text='Contact With Us',bg='#0B2F3A' ,fg='white',font=('Arial',12, 'bold'))
 Title3.place(x=25,y=90)
-B1=Button(F1,text='Account On Facebook',width=26,fg='black',bg='#DBA901',font=('Arial',11, 'bold'))
+B1=Button(F1,text='Account On Facebook',width=26,fg='black',bg='#DBA901',font=('Arial',11, 'bold'),command=Open1)
 B1.place(x=1,y=130)
-B2=Button(F1,text='Account On Telegram',width=26,fg='black',bg='#DBA901',font=('Arial',11, 'bold'))
+B2=Button(F1,text='Account On Telegram',width=26,fg='black',bg='#DBA901',font=('Arial',11, 'bold'),command=OPen2)
 B2.place(x=1,y=180)
-B3=Button(F1,text='channel On Youtube',width=26,fg='black',bg='#DBA901',font=('Arial',11, 'bold'))
+B3=Button(F1,text='channel On Youtube',width=26,fg='black',bg='#DBA901',font=('Arial',11, 'bold'),command=OPen3)
 B3.place(x=1,y=230)
-B4=Button(F1,text='Overview Of Our Project',width=26,fg='black',bg='#DBA901',font=('Arial',11, 'bold'))
+B4=Button(F1,text='Overview Of Our Project',width=26,fg='black',bg='#DBA901',font=('Arial',11, 'bold'),command=about1)
 B4.place(x=1,y=280)
 B5=Button(F1,text='Communication With Us',width=26,fg='black',bg='#DBA901',font=('Arial',11, 'bold'))
 B5.place(x=1,y=330)
-B6=Button(F1,text='Close The Website',width=26,fg='black',bg='#DBA901',font=('Arial',11, 'bold'))
-B6.place(x=1,y=380) 
+B6=Button(F1,text='Close The Website',width=26,fg='black',bg='#DBA901',font=('Arial',11, 'bold'),command=quit)
+B6.place(x=1,y=380)
+
 # تحميل وتغيير حجم الصورة
-image = Image.open(r'C:\shop.JPG')
-image = image.resize((250, 250))  # حجم مناسب للواجهة
+# تحميل وتغيير حجم الصورة
+image = Image.open(r'C:\\shop.png')
+#image = image.resize((300, 300))  # حجم مناسب للواجهة
 photo = ImageTk.PhotoImage(image)
 
-# وضع الصورة في منتصف المساحة الرمادية
-label = Label(pro, image=photo, bg='#F2F2F2')  # خلي الخلفية زي الخلفية الرمادية
-label.image = photo
-label.place(x=275, y=100)  # تقريبًا في النص (800 عرض - 250 للصورة - 230 للفريم = 320 فراغ)
+image_label = Label(pro, image=photo)
+image_label.place(x=120, y=43,width=308,height=300)  # 150 ده ارتفاع الصورة، غيريه حسب مكانك
+pro.configure(bg='white')  # Set the background color to white
+#الجزء اللي تحت 
+F2=Frame(pro,width=570,height=170,bg='#0B2F3A')
+F2.place(x=0,y=330)
+
+image2 = Image.open(r'C:\\login(1).png')
+image2 = image2.resize((70, 70))
+image2_tk = ImageTk.PhotoImage(image2)
+
+image2_label = Label(pro, image=image2_tk)
+image2_label.image = image2_tk  # مهم جدًا علشان ما يتمسحش من الـ memory
+
+image2_label.place(x=420,y=335,width=100,height=100)
+L1=Label(F2,text='username',fg='gold',bg='#0B2F3A',font=('tajawal',12))
+L1.place(x=330,y=26)
+
+L2=Label(F2,text='password ',fg='gold',bg='#0B2F3A',font=('tajawal',12))
+L2.place(x=330,y=70)
+En1=Entry(F2,font=('tajawal',12),justify='center')
+En1.place(x=130,y=26)
+En2=Entry(F2,font=('tajawal',12),justify='center')
+En2.place(x=130,y=71)
+B=Button(F2,text='login ',bg='#DBA901',font='tajawal',command=log)
+B.place(x=4,y=71)
+
+
+
 
 pro.mainloop()
